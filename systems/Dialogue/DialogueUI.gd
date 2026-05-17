8619aa634720061b40cdb96e587d48970565515c
pjc8513-hub <pjc8513@gmail.com>
2026-05-17 18:33:16 -0400

    confirmation box 2

--- systems/Dialogue/DialogueUI.gd
+++ systems/Dialogue/DialogueUI.gd
@@ -2,7 +2,7 @@ extends Control
 @onready var npc_name_label: Label = $Panel/HBoxContainer/DialogueContainer/NPCNameLabel
 @onready var dialogue_text: RichTextLabel = $Panel/HBoxContainer/DialogueContainer/DialogueText
 @onready var password_input: LineEdit = $Panel/HBoxContainer/DialogueContainer/PasswordInput
-@onready var choice_container: VBoxContainer = $Panel/HBoxContainer/ChoicesContainer
+@onready var choice_container: VBoxContainer = $Panel/HBoxContainer/ChoiceContainer
 
 var choice_button_scene = preload("res://ui/DialogueChoiceButton.tscn")
 
@@ -13,6 +13,7 @@ func _ready() -> void:
 
 
 func clear_choices():
-
+	if choice_container == null:
+		return
 	for child in choice_container.get_children():
 		child.queue_free()
