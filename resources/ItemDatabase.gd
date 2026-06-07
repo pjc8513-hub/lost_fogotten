d7b7096fc849567847af48ca28eb3fed4a0fc130
pjc8513-hub <pjc8513@gmail.com>
2026-06-07 14:34:08 -0400

    created directories for item types

--- resources/ItemDatabase.gd
+++ resources/ItemDatabase.gd
@@ -3,6 +3,7 @@ extends RefCounted
 
 static var _items: Dictionary = {}
 static var _loaded: bool = false
+const ITEM_ROOT := "res://data/items/"
 
 # Public API to get an item by its ID
 static func get_item(item_id: String) -> ItemData:
@@ -32,40 +33,55 @@ static func get_item(item_id: String) -> ItemData:
 # Scans the directory and loads every ItemData resource into memory
 static func _load_items() -> void:
 	_loaded = true
-	var dir_path = "res://data/weapons/"
+	_items.clear()
 	
-	if not DirAccess.dir_exists_absolute(dir_path):
-		push_error("ItemDatabase: Directory not found at %s" % dir_path)
+	if not DirAccess.dir_exists_absolute(ITEM_ROOT):
+		push_error("ItemDatabase: Directory not found at %s" % ITEM_ROOT)
 		return
-		
+
+	var loaded_count := _load_items_from_directory(ITEM_ROOT)
+	print("ItemDatabase: Successfully loaded %d item resources from %s." % [loaded_count, ITEM_ROOT])
+
+
+static func _load_items_from_directory(dir_path: String) -> int:
 	var dir = DirAccess.open(dir_path)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		
-		while file_name != "":
-			if not dir.current_is_dir() and file_name.ends_with(".tres"):
-				var full_path = dir_path + file_name
-				var resource = ResourceLoader.load(full_path)
-				
-				if resource is ItemData:
-					var id = resource.item_id
-					if id == "":
-						# Fallback to filename without extension
-						id = file_name.get_basename()
-					
-					# Register using various forms for bulletproof matching
-					_items[id] = resource
-					_items[id.to_lower()] = resource
-					_items[id.to_snake_case()] = resource
-					
-					var file_basename = file_name.get_basename()
-					_items[file_basename] = resource
-					_items[file_basename.to_lower()] = resource
-					_items[file_basename.to_snake_case()] = resource
-					
-			file_name = dir.get_next()
-		dir.list_dir_end()
-		print("ItemDatabase: Successfully loaded %d items from %s." % [_items.size(), dir_path])
-	else:
+	if dir == null:
 		push_error("ItemDatabase: Failed to open directory at %s" % dir_path)
+		return 0
+
+	var loaded_count := 0
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+
+	while file_name != "":
+		var full_path = dir_path.path_join(file_name)
+
+		if dir.current_is_dir():
+			loaded_count += _load_items_from_directory(full_path)
+		elif file_name.ends_with(".tres"):
+			var resource = ResourceLoader.load(full_path)
+			if resource is ItemData:
+				_register_item(resource, file_name)
+				loaded_count += 1
+
+		file_name = dir.get_next()
+
+	dir.list_dir_end()
+	return loaded_count
+
+
+static func _register_item(resource: ItemData, file_name: String) -> void:
+	var id = resource.item_id
+	if id == "":
+		# Fallback to filename without extension
+		id = file_name.get_basename()
+
+	# Register using various forms for bulletproof matching
+	_items[id] = resource
+	_items[id.to_lower()] = resource
+	_items[id.to_snake_case()] = resource
+
+	var file_basename = file_name.get_basename()
+	_items[file_basename] = resource
+	_items[file_basename.to_lower()] = resource
+	_items[file_basename.to_snake_case()] = resource
